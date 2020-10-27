e349921dcbfecaedb9ac025aab4b61d6c5475882
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-10-27 16:33:17 +0000

    Added checkout views and templates

--- judi_therapies/settings.py
+++ judi_therapies/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'judi_therapies.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
