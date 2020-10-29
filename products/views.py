aa7517219cede5f41645d5b7c4c4d7bc8ba8cfe7
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-10-29 21:30:53 +0000

    created product management function from home page as superuser enabling to add products from the site

--- products/views.py
+++ products/views.py
@@ -4,8 +4,10 @@ from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -30,7 +32,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -69,9 +71,25 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
 
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
 
-
+    return render(request, template, context)
 
 
 
