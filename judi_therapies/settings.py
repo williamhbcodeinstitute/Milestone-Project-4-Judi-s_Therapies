1d8ce6418a513f64193fb3899b4a59f3dbc53f71
williamhbcodeinstitute <william.h.barnes@outlook.com>
2020-10-30 23:18:32 +0000

    removed secret key and set debug

--- judi_therapies/settings.py
+++ judi_therapies/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5x=0scvrtl#fat5(q_p!h!mbtz!zlf2pze678^k6ri90^*wi=b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['whb-judi-therapies.herokuapp.com', 'localhost']
 
